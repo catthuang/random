7c8ac06519fae94cf7c1ab68d6c5dd31309fbd34
343444568 <catherine.huang5@student.tdsb.on.ca>
2024-01-11 13:55:32 -0500

    dsfhsd

--- src/com/company/test2.java
+++ src/com/company/test2.java
@@ -1,20 +1,33 @@
 package com.company;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
-public class test2 extends JFrame {
 
+public class test2 extends JPanel{
+    private Image image;
 
-
-    public static void main(String[] args) {
-        int[][] thing = new int[4][3];
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<3; j++) {
-                String name = "face " + Integer.toString(i*3 + j + 1);
-                System.out.print(name);
-            }
+    public test2() {
+        try {
+            image = ImageIO.read(new File("path/to/your/image.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Draw an image
+        if (image != null) {
+            g2d.drawImage(image, 300, 200, this);
+        }
+    }
 
 }
